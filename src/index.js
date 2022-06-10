8f4a4cd438991ca48ecf2a363cc84c03671f18ba
Neil <neil.bennett01@gmail.com>
2022-06-10 14:54:43 -0400

    editing second patch function

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import express from "express";
 import cors from "cors";
 import { config } from "dotenv";
-import { addARecord, getAllRecords } from "./services/actions.js";
+import { addARecord, getAllRecords, getOneRecord } from "./services/actions.js";
+import { ObjectId } from "mongodb";
+import { getConnected } from "./gateway/connectDb.js";
 
 config();
 
@@ -9,7 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.post("/", async (req, res) => {
+app.post("/diary", async (req, res) => {
   try {
     await addARecord(req.body);
     res.status(200).send("Entry added!");
@@ -18,12 +20,40 @@ app.post("/", async (req, res) => {
   }
 });
 
-app.get("/", async (req, res) => {
+app.get("/diary", async (req, res) => {
   try {
     const all = await getAllRecords();
     res.status(200).send(all);
   } catch (error) {
     res.status(500).send(error);
+  }
+});
+
+app.patch("/diary/:id", async (req, res) => {
+  const updates = req.body;
+
+  if (ObjectId.isValid(req.params.id)) {
+    const db = await getConnected();
+    db.collection("diary").updateOne(
+      { _id: ObjectId(req.params.id) },
+      { $set: updates }
+    );
+    res.status(200).send({ success: "Record updated" });
+  } else {
+    res.status(500).send("Not updated");
+  }
+});
+app.patch("/diary/patch/:id", async (req, res) => {
+  if (ObjectId.isValid(req.params.id)) {
+    const db = await getConnected();
+    db.collection("diary").updateOne(
+      { _id: ObjectId(req.params.id) },
+      { $push: { mpg: [89] } }
+      // { $push: { scores: 89 } }
+    );
+    res.status(200).send({ success: "Record updated" });
+  } else {
+    res.status(500).send("Not updated");
   }
 });
 
--- src/services/actions.js
+++ src/services/actions.js
@@ -1,3 +1,4 @@
+import { ObjectId } from "mongodb";
 import { getConnected } from "../gateway/connectDb.js";
 
 export const getDiaryCollection = async () => {
@@ -6,13 +7,27 @@ export const getDiaryCollection = async () => {
 };
 
 export const addARecord = async (rec) => {
+  const record = {
+    alias: rec.alias,
+    year: parseInt(rec.year),
+    make: rec.make,
+    model: rec.model,
+    mileage: parseInt(rec.mileage),
+    // time: new Date(),
+  };
   const col = await getDiaryCollection();
-  const { insertedId } = await col.insertOne(rec);
-  return insertedId.toString();
+  const { newRecord } = await col.insertOne(record);
+  return newRecord;
 };
 
 export const getAllRecords = async () => {
   const col = await getDiaryCollection();
-  const single = await col.find({}).toArray();
-  return single;
+  const all = await col.find({}).toArray();
+  return all;
+};
+
+export const getOneRecord = async () => {
+  const col = await getDiaryCollection();
+  const single = await col.findOne();
+  console.log(single);
 };
