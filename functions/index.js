4f284cad2f6910867caef63cf2278261a5537fa7
Neil <neil.bennett01@gmail.com>
2022-06-12 19:43:20 -0400

    cleaned up index.js file

--- functions/index.js
+++ functions/index.js
@@ -2,9 +2,13 @@ import express from "express";
 import functions from "firebase-functions";
 import cors from "cors";
 import { config } from "dotenv";
-import { addARecord, getAllRecords } from "./src/services/actions.js";
-import { ObjectId } from "mongodb";
-import { getConnected } from "./src/gateway/connectDb.js";
+import {
+  addARecord,
+  getAllRecords,
+  getOneRecord,
+  updateRecord,
+  makeARecord,
+} from "./src/services/actions.js";
 
 config();
 
@@ -12,66 +16,12 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.post("/diary", async (req, res) => {
-  try {
-    await addARecord(req.body);
-    res.status(200).send("Entry added!");
-  } catch (error) {
-    res.status(400).send("Message couldn't be added");
-  }
-});
+app.post("/diary", makeARecord);
 
-app.get("/diary", async (req, res) => {
-  try {
-    const all = await getAllRecords();
-    res.status(200).send(all);
-  } catch (error) {
-    res.status(500).send(error);
-  }
-});
+app.get("/diary", getAllRecords);
 
-app.get("/diary/:id", async (req, res) => {
-  if (ObjectId.isValid(req.params.id)) {
-    const db = await getConnected();
-    const one = await db
-      .collection("diary")
-      .findOne({ _id: ObjectId(req.params.id) });
-    res.status(200).send(one);
-  } else {
-    res.status(500).send("Not updated");
-  }
-});
+app.get("/diary/:id", getOneRecord);
 
-// app.patch("/diary/:id", async (req, res) => {
-//   const updates = req.body;
-
-//   if (ObjectId.isValid(req.params.id)) {
-//     const db = await getConnected();
-//     db.collection("diary").updateOne(
-//       { _id: ObjectId(req.params.id) },
-//       { $set: updates }
-//     );
-//     res.status(200).send({ success: "Record updated" });
-//   } else {
-//     res.status(500).send("Not updated");
-//   }
-// });
-
-app.patch("/diary/patch/:id", async (req, res) => {
-  if (ObjectId.isValid(req.params.id)) {
-    const db = await getConnected();
-    db.collection("diary").updateOne(
-      { _id: ObjectId(req.params.id) },
-      { $push: { mpg: parseFloat(req.body.mpg) } }
-    );
-    db.collection("diary").updateOne(
-      { _id: ObjectId(req.params.id) },
-      { $set: { mileage: req.body.mileage } }
-    );
-    res.status(200).send({ success: "Record updated" });
-  } else {
-    res.status(500).send("Not updated");
-  }
-});
+app.patch("/diary/:id", updateRecord);
 
 export const api = functions.https.onRequest(app);
--- functions/src/services/actions.js
+++ functions/src/services/actions.js
@@ -13,25 +13,56 @@ export const addARecord = async (rec) => {
     make: rec.make,
     model: rec.model,
     mileage: parseInt(rec.mileage),
-    // time: new Date(),
   };
   const col = await getDiaryCollection();
   const { newRecord } = await col.insertOne(record);
   return newRecord;
 };
 
-export const getAllRecords = async () => {
-  const col = await getDiaryCollection();
-  const all = await col.find({}).toArray();
-  return all;
+export const makeARecord = async (req, res) => {
+  try {
+    await addARecord(req.body);
+    res.status(200).send("Message added");
+  } catch (error) {
+    res.status(400).send("Message couldn't be added");
+  }
 };
 
-export const getOneRecord = async () => {
+export const getAllRecords = async (req, res) => {
+  try {
+    const col = await getDiaryCollection();
+    const all = await col.find({}).toArray();
+    res.status(200).send(all);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+};
+
+export const getOneRecord = async (req, res) => {
   if (ObjectId.isValid(req.params.id)) {
     const db = await getConnected();
     const one = await db
       .collection("diary")
       .findOne({ _id: ObjectId(req.params.id) });
-    return one;
+    res.status(200).send(one);
+  } else {
+    res.status(400).send("Not updated");
+  }
+};
+
+export const updateRecord = async (req, res) => {
+  if (ObjectId.isValid(req.params.id)) {
+    const db = await getConnected();
+    db.collection("diary").updateOne(
+      { _id: ObjectId(req.params.id) },
+      { $push: { mpg: parseFloat(req.body.mpg) } }
+    );
+    db.collection("diary").updateOne(
+      { _id: ObjectId(req.params.id) },
+      { $set: { mileage: req.body.mileage } }
+    );
+    res.status(200).send({ success: "Record updated" });
+  } else {
+    res.status(400).send("Not updated");
   }
 };
